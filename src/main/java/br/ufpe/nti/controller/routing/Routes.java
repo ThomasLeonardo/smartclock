67cf92d32982d6c0abcd4ca5cdaba37b187d7233
thomas leonardo <thomas leonardo>
2017-04-12 11:45:47 -0300

    Corrected bug in /clock POST route where the response would be sent to the route itself

--- src/main/java/br/ufpe/nti/controller/routing/Routes.java
+++ src/main/java/br/ufpe/nti/controller/routing/Routes.java
@@ -14,6 +14,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -51,9 +52,10 @@ public class Routes
     }
     
     @RequestMapping(value = "/clock", method = RequestMethod.POST)
-    public void postClock()
+    @ResponseBody
+    public String postClock()
     {
-        
+        return "hello!";
     }
     
 }
